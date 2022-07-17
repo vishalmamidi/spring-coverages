9f5f22157cb1fe11cd18e0541420d4c80c74db02
vishalmamidi <mamidivishalsa@gmail.com>
2022-07-17 06:28:35 +0530

    fixed coverage issue for gradle

---
+++ src/main/java/com/vishalmamidi/springcoverage/models/ResponseModel.java
@@ -0,0 +1,11 @@
+package com.vishalmamidi.springcoverage.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ResponseModel {
+    int name;
+    int responseCode;
+}
